3ca4aa6be442430ee28fb6b64ca44483a287a8a8
Krasty <krasty_90@mail.ru>
2020-06-10 23:40:43 +0300

    add2

---
+++ ex18/ex18.sql
@@ -0,0 +1,7 @@
+SELECT `name` FROM `distrib`
+WHERE (`id_distrib` = 42
+	OR (`id_distrib` < 70 AND `id_distrib` > 61)
+	OR `id_distrib` = 71
+	OR (`id_distrib` < 91 AND `id_distrib` > 87)
+	OR name LIKE '%y%y%')
+LIMIT 2, 5;
